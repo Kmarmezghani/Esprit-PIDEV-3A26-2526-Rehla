95b6bcc0772e960c470d02958211c949abfeac60
yosr2003 <yosr.amamou@yahoo.com>
2026-02-12 20:19:44 +0100

    ..

--- src/main/java/rehla/MainFx.java
+++ src/main/java/rehla/MainFx.java
@@ -1,14 +1,14 @@
 package rehla;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MainFx extends Application {
-
+    double x,y =0;
     public static void main(String[] args) {
         launch(args);
     }
@@ -16,9 +16,18 @@ public class MainFx extends Application {
     @Override
     public void start(Stage primaryStage) {
         try{
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/AjouterActivite.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Dashboard.fxml"));
             Parent root = fxmlLoader.load();
             Scene scene = new Scene(root);
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+            root.setOnMousePressed(event -> {
+                x = event.getSceneX();
+                y = event.getSceneY();
+            });
+            root.setOnMouseDragged(event -> {
+                primaryStage.setX(event.getScreenX() - x);
+                primaryStage.setY(event.getScreenY() - y);
+            });
             primaryStage.setScene(scene);
             primaryStage.show();
         }

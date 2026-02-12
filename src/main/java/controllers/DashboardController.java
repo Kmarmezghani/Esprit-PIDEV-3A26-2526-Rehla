fc5d7094f708131fb53bb027d54f4c25d0765024
yosr2003 <yosr.amamou@yahoo.com>
2026-02-12 20:27:28 +0100

    ..

--- src/main/java/controllers/DashboardController.java
+++ src/main/java/controllers/DashboardController.java
@@ -1,22 +1,12 @@
-package controllers;
+package Controllers;
 
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-
-
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 
 public class DashboardController {

7721db5cb8364a2f8bcaf5de9c0f0a1aca5fe02e
yosr2003 <yosr.amamou@yahoo.com>
2026-02-20 14:46:47 +0100

    ................

--- src/main/java/rehla/MainFx.java
+++ src/main/java/rehla/MainFx.java
@@ -16,9 +16,9 @@ public class MainFx extends Application {
     @Override
     public void start(Stage primaryStage) {
         try{
-            //FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Dashboard.fxml"));
-            //  FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/blogAllPosts.fxml"));
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/BlogProfileView.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Dashboard.fxml"));
+            // FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/blogAllPosts.fxml"));
+            // FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/BlogProfileView.fxml"));
             Parent root = fxmlLoader.load();
             Scene scene = new Scene(root);
            scene.getStylesheets().add(getClass().getResource("/css/blog_styles.css").toExternalForm());

7e4d073a7c9f9a622d3f0ee93e30a0a2285c528f
kmarmezghani <kmar.mezghani@gmail.com>
2026-04-08 19:23:40 +0100

    crud activite

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -31,32 +31,38 @@ final class AdminController extends AbstractController
         ]);
     }
 
-    
     #[Route('/admin/activite/new', name: 'activite_new')]
     public function activiteNew(Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
     {
         $activite = new Activite();
 
-        $form = $this->createForm(ActiviteType::class, $activite);
+        $form = $form = $this->createForm(ActiviteType::class, $activite, [
+    'is_edit' => false,
+]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $imageFile = $form->get('image')->getData();
+
             if ($imageFile) {
+               
                 $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
                 $safeFilename = $slugger->slug($originalFilename);
                 $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
 
+                
+                $destinationPath = $this->getParameter('activities_directory');
+
                 try {
-                    $imageFile->move(
-                        $this->getParameter('activities_directory'),
-                        $newFilename
-                    );
+                    
+                    $imageFile->move($destinationPath, $newFilename);
                 } catch (\Exception $e) {
                     $this->addFlash('danger', 'Erreur lors de l\'upload de l\'image');
                 }
 
-                $activite->setImage($newFilename);
+               
+                $physicalPath = $destinationPath . '\\' . $newFilename;
+                $activite->setImage($physicalPath);
             }
 
             $em->persist($activite);
@@ -70,5 +76,58 @@ final class AdminController extends AbstractController
         return $this->render('admin/activite_new.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+    #[Route('/admin/activite/{id}/edit', name: 'activite_edit')]
+    public function edit(Request $request, Activite $activite, EntityManagerInterface $em, SluggerInterface $slugger): Response
+    {
+        $form = $this->createForm(ActiviteType::class, $activite, [
+            'is_edit' => true,
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            
+            $imageFile = $form->get('image')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                $destinationPath = $this->getParameter('activities_directory');
+
+                try {
+                    $imageFile->move($destinationPath, $newFilename);
+                } catch (\Exception $e) {
+                    $this->addFlash('danger', 'Erreur lors de l\'upload de l\'image');
+                }
+
+                $physicalPath = $destinationPath . '\\' . $newFilename;
+                $activite->setImage($physicalPath);
+            }
+            
+
+            $em->flush(); 
+
+            $this->addFlash('success', 'Activité modifiée avec succès !');
+
+            return $this->redirectToRoute('activite_admin');
+        }
+
+        return $this->render('admin/activite_edit.html.twig', [
+            'form' => $form->createView(),
+            'activite' => $activite
+        ]);
+    }
+    #[Route('/admin/activite/{id}/delete', name: 'activite_delete')]
+    public function delete(Activite $activite, EntityManagerInterface $em): Response
+    {
+        $em->remove($activite);
+        $em->flush();
+
+        $this->addFlash('success', 'Activité supprimée avec succès !');
+
+        return $this->redirectToRoute('activite_admin');
     }
 }

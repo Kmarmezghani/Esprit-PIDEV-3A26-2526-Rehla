3bfa0226a52f726649a8c69685f95ea56a438622
yosr2003 <yosr.amamou@yahoo.com>
2026-04-08 22:11:03 +0100

    add post

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -5,7 +5,7 @@ namespace App\Form;
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Length;
@@ -16,7 +16,7 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('contenu', TextareaType::class, [
+            ->add('contenu', TextType::class, [
                 'required' => true,
                 'constraints' => [
                     new NotBlank(['message' => 'Le contenu de la publication est obligatoire']),
